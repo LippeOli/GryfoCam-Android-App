7a672accc5d6790c385783c1ed35e2c426a784f5
LippeOli <lipe060672@gmail.com>
2024-05-12 23:51:41 -0300

    converter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,28 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.tracing.perfetto.handshake)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    //CameraX dependencies
+    val camerax_version = "1.3.3"
+    implementation ("androidx.camera:camera-core:${camerax_version}")
+    implementation ("androidx.camera:camera-camera2:${camerax_version}")
+    implementation ("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation ("androidx.camera:camera-video:${camerax_version}")
+
+    implementation ("androidx.camera:camera-view:${camerax_version}")
+    implementation ("androidx.camera:camera-extensions:${camerax_version}")
+
+
+    //Retrofit dependencies
+    implementation ("com.google.code.gson:gson:2.8.7")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.activity:activity-ktx:1.3.1")
+
+
 }
--- app/src/main/java/com/gryfoapp/MainActivity.kt
+++ app/src/main/java/com/gryfoapp/MainActivity.kt
@@ -1,78 +1,68 @@
 package com.gryfoapp
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.io.ByteArrayOutputStream
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var captureIV1: ImageView
-    private lateinit var captureIV2: ImageView
-    private lateinit var imageUrl1: Uri
-    private lateinit var imageUrl2: Uri
-    private var photoCount = 0
+    private lateinit var capture1 : ImageView
+    private lateinit var imageUrl : Uri
+    private lateinit var convertTextView: TextView
+    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()){
 
-    private var contract: ActivityResultLauncher<Uri>? = null // Inicializamos a variável como nula
+        capture1.setImageURI(null)
+        capture1.setImageURI(imageUrl)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        captureIV1 = findViewById(R.id.captureImageView1)
-        captureIV2 = findViewById(R.id.captureImageView2)
-
+        imageUrl = createImageUri()
+        capture1 = findViewById(R.id.captureImageView1)
+        convertTextView = findViewById(R.id.convertTextView)
         val captureImageButton = findViewById<Button>(R.id.captureImageButton)
         captureImageButton.setOnClickListener {
-            if (photoCount < 2) {
-                if (imageUrl1 == Uri.EMPTY) {
-                    imageUrl1 = createImageUri()
-                    contract?.launch(imageUrl1)
-                } else if (imageUrl2 == Uri.EMPTY) {
-                    imageUrl2 = createImageUri()
-                    contract?.launch(imageUrl2)
-                }
-            }
+            contract.launch(imageUrl)
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        imageUrl1 = Uri.EMPTY
-        imageUrl2 = Uri.EMPTY
-
-        contract = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
-            if (success) {
-                val currentImageView = if (captureIV1.drawable == null) captureIV1 else captureIV2
-                currentImageView.setImageURI(imageUrl1)
-
-                if (currentImageView == captureIV1) {
-                    imageUrl1 = Uri.parse(imageUrl1.toString())
-                } else {
-                    imageUrl2 = Uri.parse(imageUrl2.toString())
-                }
-
-                photoCount++
-            }
+        val buttonConvert = findViewById<Button>(R.id.buttonConvert)
+        buttonConvert.setOnClickListener {
+            val bitmap = bitMapConvertor()
+            val base64String = base64Convertor(bitmap)
+            Log.d("base64Converted", "Base 64 encoded : $base64String")
+            convertTextView.text = base64String
         }
     }
 
+    private fun bitMapConvertor() : Bitmap {
+        val drawable = capture1.drawable as BitmapDrawable
+        return drawable.bitmap
+    }
 
+    private fun base64Convertor(bitmap: Bitmap) : String {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream)
+        val imageBase = stream.toByteArray()
+        return Base64.encodeToString(imageBase,Base64.DEFAULT)
+    }
 
-    private fun createImageUri(): Uri {
-        val image = File(filesDir, "camera_photos_${System.currentTimeMillis()}.png")
-        return FileProvider.getUriForFile(this, "com.gryfoapp.FileProvider", image)
+    private fun createImageUri():Uri {
+        val image = File(filesDir,"camera_photos.png")
+        return FileProvider.getUriForFile(this,
+            "com.gryfoapp.FileProvider",
+            image)
     }
 }
